18698171c3d45ac79b8d5a2435b88d1a0a725e2d
JosÃ© da Silva Rodrigues <jsr@jose-rodrigues.info>
2021-12-17 23:11:50 +0000

    first commit

---
+++ src/data.js
@@ -0,0 +1,38 @@
+import { v4 as uuidv4 } from "uuid";
+function trash2021() {
+  return [
+    {
+      name: "Intro",
+      cover:
+        "/covers/default.jpg",
+      artist: "Afredo Fraggins",
+      audio: "/songs/intro.mp3",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: "Psy",
+      cover:
+      "/covers/default.jpg",
+      artist: "Afonso Manso",
+      audio: "/songs/psytrance.mp3",
+      color: ["#EF8EA9", "#ab417f"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Bathuska - Litourgiya COVER",
+      cover:
+      "/covers/default.jpg",
+      artist: "Rodanov Vitseti",
+      audio: "/songs/bathuska.mp3",
+      color: ["#EF8EA9", "#ab417f"],
+      id: uuidv4(),
+      active: false,
+    },
+    //ADD MORE HERE
+  ];
+}
+
+export default trash2021;
