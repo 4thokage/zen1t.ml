410a44e1cfdd57228f26ef8b1b6929a89945198c
JosÃ© da Silva Rodrigues <jsr@jose-rodrigues.info>
2021-12-18 14:24:23 +0000

    Add more songs

--- src/data.js
+++ src/data.js
@@ -22,11 +22,51 @@ function trash2021() {
       active: false,
     },
     {
-      name: "Bathuska - Litourgiya COVER",
+      name: "Bathuska - Litourgiya microCOVER",
       cover:
       "/covers/default.jpg",
       artist: "Rodanov Vitseti",
       audio: "https://zen1t-ml.herokuapp.com/songs/bathuska.mp3",
+      color: ["#EF8EA9", "#ab417f"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Gorillaz - Kids with guns microCOVER",
+      cover:
+      "/covers/default.jpg",
+      artist: "Macaco Donald",
+      audio: "https://zen1t-ml.herokuapp.com/songs/kids.mp3",
+      color: ["#EF8EA9", "#ab417f"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Hades - Tartarus microCOVER",
+      cover:
+      "/covers/default.jpg",
+      artist: "Macaco Donald",
+      audio: "https://zen1t-ml.herokuapp.com/songs/tartarus.mp3",
+      color: ["#EF8EA9", "#ab417f"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Haken - Cockroach King microCOVER",
+      cover:
+      "/covers/default.jpg",
+      artist: "Reinaldo Barata",
+      audio: "https://zen1t-ml.herokuapp.com/songs/cockroach.mp3",
+      color: ["#EF8EA9", "#ab417f"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Adiafa - Meninas da ribeira do sado microCOVER",
+      cover:
+      "/covers/default.jpg",
+      artist: "Arlete Silva, Miss ribeira do sado 2021",
+      audio: "https://zen1t-ml.herokuapp.com/songs/finas.mp3",
       color: ["#EF8EA9", "#ab417f"],
       id: uuidv4(),
       active: false,
