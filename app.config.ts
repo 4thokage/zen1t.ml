128f8e9e6a8ade98f12bc3982b4f135591b6a909
Jose da Silva Rodrigues <jsr@jose-rodrigues.info>
2024-04-15 23:29:50 +0100

    Fix typo in cfPages deployment config

--- app.config.ts
+++ app.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from "@solidjs/start/config";
 
 export default defineConfig({
   server: {
-    preset: "cloudflare_module",
-    rollupConfig: {
-      external: ["__STATIC_CONTENT_MANIFEST", "node:async_hooks"],
-    },
+    preset: "cloudflare_pages",
   },
 });
